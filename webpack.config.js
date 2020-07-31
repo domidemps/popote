cf99012a26df0d14581912e4b532a27ab24a3c3e
domidemps <domitille.dempure@gmail.com>
2020-07-31 19:36:54 +0200

    #18 - Add Prettier, JavaScript, Webpack config files and yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,72 @@
-const webpack = require('webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+module.exports = async (env, argv) => {
+  const isOnline = await require('is-online')
+  return {
     context: __dirname,
-    entry: ["./src/index.jsx", "./src/index.html"],
+    entry: ['./src/index.jsx'],
     devServer: {
-        contentBase: __dirname + "/static",
-        historyApiFallback: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
-                secure: false
-            },
-            '/logout': {
-                target: 'http://localhost:5000',
-                secure: false
-            }
-        }
+      contentBase: __dirname + '/static',
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          secure: false,
+        },
+        '/api/logout': {
+          target: 'http://localhost:5000',
+          secure: false,
+        },
+      },
     },
     output: {
-        path: __dirname + "/static",
-        filename: "bundle.js",
-        globalObject: 'this'
+      path: __dirname + '/static',
+      filename: 'bundle.js',
+      globalObject: 'this',
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-        new CompressionPlugin({
-            test: /\.js(\?.*)?$/i,
-            algorithm: 'gzip'
-            })
+      new CompressionPlugin({
+        test: /\.js(\?.*)?$/i,
+        algorithm: 'gzip',
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        title: 'popote',
+        template: './src/index.ejs',
+        appMountId: 'root',
+        isOnline,
+      }),
     ],
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+          use: 'url-loader?name=[path][name]-[hash:8].[ext]',
+        },
+        {
+          test: /\.css$/,
+          use: [
             {
-                test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-                use: "url-loader?name=[path][name]-[hash:8].[ext]"
+              loader: 'style-loader',
             },
             {
-                test: /\.html$/,
-                use: "file-loader?name=[name].[ext]"
+              loader: 'css-loader',
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: ["babel-loader",
-                    {
-                        loader: "eslint-loader",
-                        options: {configFile: __dirname + "/.eslintrc"}
-                    }]
-            }
-        ]
+          ],
+        },
+        {
+          test: /\.(js|ts)x?$/,
+          use: 'babel-loader',
+        },
+      ],
     },
     resolve: {
-        extensions: ['.js', '.jsx']
-    }
-};
+      modules: ['node_modules'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+      },
+      extensions: ['.js', '.jsx'],
+    },
+  }
+}
