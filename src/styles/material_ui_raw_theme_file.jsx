b2b4bef9e60003cd477ff6b14c598f7da7e1ce25
domidemps <domitille.dempure@gmail.com>
2020-12-17 00:56:36 +0100

    #7 - Login view

--- src/styles/material_ui_raw_theme_file.jsx
+++ src/styles/material_ui_raw_theme_file.jsx
@@ -1,8 +1,32 @@
 import {createMuiTheme} from '@material-ui/core/styles'
+import {darken, lighten, fade} from '@material-ui/core'
+
+export const LIGHT_YELLOW = '#eaf8bf'
+export const MEDIUM_YELLOW = '#e7eb90'
+export const INTENSE_YELLOW = '#eca400'
+export const MEDIUM_PURPLE = '#7f055f'
+export const DARK_PURPLE = '#45062e'
+export const WHITE = '#ffffff'
 
 const rawBaseTheme = {
   palette: {
-    type: 'dark',
+    background: {
+      paper: fade(LIGHT_YELLOW, 0.6),
+    },
+    primary: {
+      main: MEDIUM_PURPLE,
+      contrastText: WHITE,
+    },
+  },
+  overrides: {
+    MuiButton: {
+      containedPrimary: {
+        backgroundColor: MEDIUM_PURPLE,
+        '&:hover': {
+          backgroundColor: lighten(MEDIUM_PURPLE, 0.2),
+        },
+      },
+    },
   },
 }
 
