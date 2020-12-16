d2dfcaae558fa029269bcd5ad08e9a56aed8efdc
ldehaine <louyann.dehaine@gmail.com>
2020-12-16 20:25:33 +0100

    Update init_db for compatibility with UNIX-based systems

--- python/init_db.py
+++ python/init_db.py
@@ -5,5 +5,5 @@ from pony.orm import Database
 
 def init_db(db: Database):
     db_password = os.environ["POPOTE_DB_PASSWORD"]
-    db.bind(provider="postgres", user="popote_team", password=db_password, database="popote")
+    db.bind(provider="postgres", user="popote_team", password=db_password, database="popote", host="localhost")
     db.generate_mapping(create_tables=True)
