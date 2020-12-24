c97c877012f8437bc60cd3a8f056759580554de6
ldehaine <36965333+ldehaine@users.noreply.github.com>
2020-12-24 16:07:52 +0100

    [Backend] Add CORS middleware

--- python/main.py
+++ python/main.py
@@ -1,12 +1,26 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from api import recipes, users, token
 from init_db import init_db
 from models import db
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
