66d7a4f6e942e79c9667acd0ce72016278bfe2cb
domidemps <domitille.dempure@gmail.com>
2020-12-29 00:49:14 +0100

    #10 - Add main page and logout button

---
+++ src/containers/MainView.jsx
@@ -0,0 +1,51 @@
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core'
+import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+
+import {INTENSE_YELLOW, MEDIUM_PURPLE, DARK_PURPLE} from 'styles/material_ui_raw_theme_file'
+import AddNotes from 'images/add_notes.svg'
+
+const styles = css`
+  .appBar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .body {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+  .signOut {
+    color: white;
+  }
+  .message {
+    color: ${DARK_PURPLE};
+    margin: 40px;
+    text-align: center;
+  }
+`
+
+export default function MainView() {
+  return (
+    <div css={styles}>
+      <AppBar position="sticky" className="appBar" color="secondary">
+        <IconButton color="primary" aria-label="sign out">
+          <ExitToAppIcon className="signOut" />
+        </IconButton>
+      </AppBar>
+      <div className="body">
+        <img src={AddNotes} alt="Ajouter une recette" />
+        <Typography variant="h4" align="center" className="message">
+          Le carnet de recette est vide !
+        </Typography>
+      </div>
+    </div>
+  )
+}
