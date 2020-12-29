d99d28c483476a7cf8662d6e7541032c6c0c0731
ldehaine <louyann.dehaine@gmail.com>
2020-12-29 13:37:54 +0100

    [Backend] Update GET users/me to return useful info

--- python/api/users.py
+++ python/api/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, Security
-from pony.orm import db_session
+from fastapi import APIRouter, HTTPException, Security
+from pony.orm import TransactionIntegrityError, db_session
 
-from security.oauth import get_current_active_user, get_password_hash
 from models.user import User
+from security.oauth import get_current_active_user, get_password_hash
 
 router = APIRouter()
 
@@ -14,26 +14,36 @@ async def read_user(email: str) -> str:
     return user.name
 
 
+@router.get("/users")
+async def read_users(_: User = Security(get_current_active_user, scopes=["users:read"])) -> list[dict[str, str]]:
+    with db_session:
+        users = User.select()
+        return [user.to_dict() for user in users]
+
+
 @router.post("/users")
 def create_user(name: str, email: str, password: str) -> str:
     hashed_password = get_password_hash(password)
-    with db_session:
-        User(name=name, email=email, password=hashed_password)
+    try:
+        with db_session:
+            User(name=name, email=email, password=hashed_password)
+    except TransactionIntegrityError as e:
+        raise HTTPException(
+            status_code=409, detail=f"A user with the same email address ({email}) already exists in the database."
+        ) from e
     return hashed_password
 
 
 @router.get("/users/me")
-async def read_user_me(current_user: User = Security(get_current_active_user, scopes=["user:read"])):
-    # TODO: Return a JSON containing username, email...
-    return current_user
+async def read_user_me(current_user: User = Security(get_current_active_user, scopes=["users:read"])) -> dict:
+    return current_user.to_dict(exclude=("uuid", "password"))
 
 
 @router.post("/users/me")
-@db_session
-def update_user_name(new_name: str, current_user_uuid: str = Security(get_current_active_user, scopes=["user:write"])):
-    user = User[current_user_uuid]
-    user.name = new_name
-    return user.to_dict()
+def update_user_name(new_name: str, user: User = Security(get_current_active_user, scopes=["users:write"])) -> str:
+    with db_session:
+        user.name = new_name
+    return "Username updated successfully"
 
 
 @router.delete("/users")
