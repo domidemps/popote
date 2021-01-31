4c732a348fdc6e6fa4e7e257bcb12b5b2737ca54
ldehaine <louyann.dehaine@gmail.com>
2021-01-31 19:21:08 +0100

    support connection to remote db

--- python/init_db.py
+++ python/init_db.py
@@ -4,12 +4,12 @@ from security.credentials import get_credentials
 
 
 def init_db(db: Database):
-    db_credentials = get_credentials(path="db/db_account")
+    db_credentials = get_credentials(path="db/account", fields=("username", "password", "url", "database_name"))
     db.bind(
         provider="postgres",
         user=db_credentials["username"],
         password=db_credentials["password"],
-        database="popote",
-        host="localhost",
+        database=db_credentials["database_name"],
+        host=db_credentials["url"],
     )
     db.generate_mapping(create_tables=True)
